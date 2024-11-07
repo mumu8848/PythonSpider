f0ccfde2664587d18ae2c9ff1b7adaab8c8a6158
mumu8848 <mumu88008@163.com>
2024-11-07 13:56:22 +0800

    测试 urllib3 库，增加请求头

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import urllib3
 
 http = urllib3.PoolManager()
-rq = http.request('GET','http://baidu.com')
+ua = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
+rq = http.request('GET','http://baidu.com',headers = ua)
 print("服务器状态码：",rq.status)
 print("响应实体：",rq.data)
