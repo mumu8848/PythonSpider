9336d032ce0ef617cd4ef55f835ebe672f630acb
mumu8848 <mumu88008@163.com>
2024-11-07 18:22:54 +0800

    测试 requests 库，调试 lxml 库的 etree 模块

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import requests
-import chardet
+from lxml import etree
 
 url = 'http://www.baidu.com'
-ua = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+ua = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
                    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
 
-rqg = requests.get(url,headers = ua,timeout = 2) # timeout若设置为0.001，会因为时间过短而报错
+rqg = requests.get(url,headers = ua)
+rqg.encoding = 'utf-8'
+html = etree.HTML(rqg.text,parser=etree.HTMLParser(encoding='utf-8'))
 
-print('状态码：',rqg.status_code)
+result = etree.tostring(html,encoding='utf-8',pretty_print=True,method='html')
 
-rqg.encoding = chardet.detect(rqg.content)['encoding']
-
-print('修改后的编码：',rqg.encoding)
-
-print('响应头：',rqg.headers)
-print('网页内容：',rqg.text)
+print('修正后的HTML对象：',result)
