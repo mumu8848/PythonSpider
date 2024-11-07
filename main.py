b5fed43847c266b278b3501581ebd7f07be9bc64
mumu8848 <mumu88008@163.com>
2024-11-07 19:01:17 +0800

    调试 lxml 库的 etree 模块，测试 xpath 谓语表达式

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from lxml import etree
 
-url = 'http://www.baidu.com'
+url = 'http://tipdm.com/'
 ua = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
                    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
 
@@ -11,15 +11,10 @@ html = etree.HTML(rqg.text,parser=etree.HTMLParser(encoding='utf-8'))
 
 result = etree.tostring(html,encoding='utf-8',pretty_print=True,method='html')
 
-result0 = html.xpath('head')
-print('名称定位 head 节点结果：',result0)
+result0 = html.xpath('//header[@class]')
+print('class 属性定位结果：',result0)
 
-result1 = html.xpath('/html/head/title')
-print('节点层级定位结果：',result1)
+result1 = html.xpath('//ul[@id="menu"]')
+print('id 属性定位结果：',result1)
 
-result2 = html.xpath('title')
-print('名称定位 title 节点结果：',result2)#
-
-result3 = html.xpath('//title')
-print('搜索定位 title 节点结果：',result3)
 
