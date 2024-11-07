7d9ea2e0e9a516b6f4dc04c6b2cb1d08eebc2122
mumu8848 <mumu88008@163.com>
2024-11-07 14:44:16 +0800

    测试 urllib3 库，第二种timeout的使用方式

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import urllib3
 
-http = urllib3.PoolManager()
+'''第二种timeout的使用方式'''
+http = urllib3.PoolManager(timeout=3.0)
 url = 'http://baidu.com'
 ua = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
-rq = http.request('GET',url,headers=ua,timeout=3.0)
+rq = http.request('GET',url,headers=ua)
+
 print("服务器状态码：",rq.status)
 print("响应实体：",rq.data)
