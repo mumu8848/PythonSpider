e5ed8b53816d9d2f2501a02968f477a015cc990d
mumu8848 <mumu88008@163.com>
2024-11-07 15:51:18 +0800

    测试 requests 库，手动指定 utf-8 编码

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import requests
 
 url = 'http://www.baidu.com'
 rqg = requests.get(url)
+rqg.encoding = 'utf-8'
 
 print('结果类型：',type(rqg))
 print('状态码：',rqg.status_code)
-print('编码：',rqg.encoding)
+print('修改后的编码：',rqg.encoding)
 print('响应头：',rqg.headers)
 print('网页内容：',rqg.text)
