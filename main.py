9dff1e03bec42aed2c97d601da9dacc6c0c277fd
mumu8848 <mumu88008@163.com>
2024-11-07 16:17:30 +0800

    测试 requests 库，使用 timeout 参数

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import requests
 import chardet
 
 url = 'http://www.baidu.com'
-rqg = requests.get(url)
+ua = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
+
+rqg = requests.get(url,headers = ua,timeout = 2) # timeout若设置为0.001，会因为时间过短而报错
 rqg.encoding = 'utf-8'
 
 print('结果类型：',type(rqg))
@@ -13,3 +16,5 @@ print('detect()方法检测结果：',chardet.detect(rqg.content))
 rqg.encoding = chardet.detect(rqg.content)['encoding']
 
 print('改变后的编码：',rqg.encoding)
+print('响应头：',rqg.headers)
+print('网页内容：',rqg.text)
