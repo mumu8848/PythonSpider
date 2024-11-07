ccd06417773f736c8010415c5e92d4f790c62671
mumu8848 <mumu88008@163.com>
2024-11-07 14:57:37 +0800

    测试 urllib3 库，完整的HTTP请求

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ http = urllib3.PoolManager()
 url = 'http://baidu.com'
 ua = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
-rq = http.request('GET',url,headers=ua,
-                  timeout = urllib3.Timeout(connect = 1.0,read = 3.0),retries = 5,redirect = 4) #设置5次请求重试以及4次重定向
+tm = urllib3.Timeout(connect = 1.0,read = 3.0)
+rq = http.request('GET',url,headers=ua,timeout = tm,retries = 5,redirect = 4) #设置5次请求重试以及4次重定向
 
 print("服务器状态码：",rq.status)
-print("响应实体：",rq.data)
+print("响应实体：",rq.data.decode('utf-8'))
