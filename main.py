4a1fddd42a140d4cc9cdb3f6a130ff4df9971653
mumu8848 <mumu88008@163.com>
2024-11-08 14:31:03 +0800

    调试 lxml 库的 etree 模块，提取ul节点下的所有文本内容和对应链接(52破解网站首页案例)

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
 import requests
 from lxml import etree
 
-url = 'http://www.xinhuanet.com/'
+url = 'http://www.52pojie.cn/'
 ua = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
                    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
 
 rqg = requests.get(url,headers = ua)
-rqg.encoding = 'utf-8'
-html = etree.HTML(rqg.text,parser=etree.HTMLParser(encoding='utf-8'))
+rqg.encoding = 'gbk'
+html = etree.HTML(rqg.text,parser=etree.HTMLParser(encoding='gbk'))
 
 result = etree.tostring(html,encoding='utf-8',pretty_print=True,method='html')
 
-result0 = html.xpath('//div[starts-with(@id,"h")]')
-print('id值以 a 开头的div节点：',result0)
-
-result1 = html.xpath('//div[contains(@id,"a")]')
-print('id值包含 a 的div节点：',result1)
-
-result2 = html.xpath('//div[contains(@id,"a") and contains(@id,"c")]')
-print('id值包含 a 和 c 的div节点：',result2)
-
-result3 = html.xpath('//div[contains(text(),"如")]')
-print('文本包含 "如" 的div节点：',result3)
+content = html.xpath('//ul[starts-with(@id,"mn")]/li//a/text()')
+for i in content:
+    print(i)
 
 
