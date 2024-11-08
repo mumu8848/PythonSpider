5b827a6a4ed9197a259dfe638bc7c3cae3b4b487
mumu8848 <mumu88008@163.com>
2024-11-08 12:23:16 +0800

    调试 lxml 库的 etree 模块，测试 Xpath 常用的谓语表达式2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from lxml import etree
 
-url = 'http://tipdm.com/'
+url = 'http://www.baidu.com'
 ua = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
                    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
 
@@ -11,10 +11,21 @@ html = etree.HTML(rqg.text,parser=etree.HTMLParser(encoding='utf-8'))
 
 result = etree.tostring(html,encoding='utf-8',pretty_print=True,method='html')
 
-result0 = html.xpath('//header[@class]')
-print('class 属性定位结果：',result0)
+result0 = html.xpath('/html/body/div[1]')
+print('第一个div节点：',result0)
 
-result1 = html.xpath('//ul[@id="menu"]')
-print('id 属性定位结果：',result1)
+result1 = html.xpath('/html/body/div[last()]')
+print('最后一个div节点：',result1)
+
+result2 = html.xpath('/html/body/div[last()-1]')
+print('倒数第二个div节点：',result2)
+
+result3 = html.xpath('/html/body/div[position()<3]')
+print('body子节点的前两个div节点：',result3)
+
+result4 = html.xpath('/html/body/div[@id]')
+print('body子节点的带有id属性的div节点：',result4)
+
+
 
 
