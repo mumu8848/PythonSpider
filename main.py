b573a146489e80700068e0b02271f4d8acc46699
mumu8848 <mumu88008@163.com>
2024-11-08 15:54:24 +0800

    测试 BeautifulSoup 库，并将网页内容转化成 BeautifulSoup 对象来格式化输出

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+from bs4 import BeautifulSoup
 import requests
-from lxml import etree
+import chardet
 
 url = 'http://www.52pojie.cn/'
 ua = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
                    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
 
 rqg = requests.get(url,headers = ua)
-rqg.encoding = 'gbk'
-html = etree.HTML(rqg.text,parser=etree.HTMLParser(encoding='gbk'))
+print('网站编码：',rqg.encoding)
+rqg.encoding = chardet.detect(rqg.content)['encoding']
 
-result = etree.tostring(html,encoding='utf-8',pretty_print=True,method='html')
 
-content = html.xpath('//ul[starts-with(@id,"mn")]/li//a/text()')
-for i in content:
-    print(i)
+html = rqg.text
+soup = BeautifulSoup(html,'lxml')
 
+print('输出格式化的 BeautifulSoup 对象：',soup.prettify())
 
