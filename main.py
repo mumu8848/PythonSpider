fc446516a1219afe7ef7ed081176aee70799b670
mumu8848 <mumu88008@163.com>
2024-11-08 14:16:14 +0800

    调试 lxml 库的 etree 模块，更新 Xpath 常用的功能函数

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from lxml import etree
 
-url = 'http://www.baidu.com'
+url = 'http://www.xinhuanet.com/'
 ua = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
                    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
 
@@ -11,21 +11,16 @@ html = etree.HTML(rqg.text,parser=etree.HTMLParser(encoding='utf-8'))
 
 result = etree.tostring(html,encoding='utf-8',pretty_print=True,method='html')
 
-result0 = html.xpath('/html/body/div[1]')
-print('第一个div节点：',result0)
+result0 = html.xpath('//div[starts-with(@id,"h")]')
+print('id值以 a 开头的div节点：',result0)
 
-result1 = html.xpath('/html/body/div[last()]')
-print('最后一个div节点：',result1)
+result1 = html.xpath('//div[contains(@id,"a")]')
+print('id值包含 a 的div节点：',result1)
 
-result2 = html.xpath('/html/body/div[last()-1]')
-print('倒数第二个div节点：',result2)
+result2 = html.xpath('//div[contains(@id,"a") and contains(@id,"c")]')
+print('id值包含 a 和 c 的div节点：',result2)
 
-result3 = html.xpath('/html/body/div[position()<3]')
-print('body子节点的前两个div节点：',result3)
-
-result4 = html.xpath('/html/body/div[@id]')
-print('body子节点的带有id属性的div节点：',result4)
-
-
+result3 = html.xpath('//div[contains(text(),"如")]')
+print('文本包含 "如" 的div节点：',result3)
 
 
