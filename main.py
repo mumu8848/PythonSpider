773fd89689f12780df406b6286f50de9f89367b3
mumu8848 <mumu88008@163.com>
2024-11-08 18:25:11 +0800

    测试 BeautifulSoup 库，Tag 对象的 attributes 属性

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import chardet
 
-url = 'http://www.baidu.cn/'
+url = 'http://www.baidu.com/'
 ua = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
                    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
 
@@ -14,10 +14,17 @@ rqg.encoding = chardet.detect(rqg.content)['encoding']
 html = rqg.text
 soup = BeautifulSoup(html,'lxml')
 
-print('BeautifulSoup对象的name属性是：',soup.name)
-
 tag = soup.a
 print('Tag对象的name属性是：',tag)
+print('Tag对象的全部属性是：',tag.attrs)
+print('class 属性的值：',tag['class'])
 
-tag.name = 'b'
-print('修改name属性后的Tag对象的内容是：',tag)
+tag['class'] = 'Toindex'
+print('修改后 Tag 对象的属性：',tag.attrs)
+
+tag['id'] = ['toindex']
+print('修改后 Tag 对象的内容：',tag)
+
+del tag['class']
+print('删除 class 后 Tag 对象的内容：',tag)
+
