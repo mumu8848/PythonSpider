3e53d786241cee8b42a78dce4b9ed47cb162e3fb
mumu8848 <mumu88008@163.com>
2024-11-11 12:57:33 +0800

    测试 BeautifulSoup 库，使用 select() 方法

--- main.py
+++ main.py
@@ -14,24 +14,18 @@ rqg.encoding = chardet.detect(rqg.content)['encoding']
 html = rqg.text
 soup = BeautifulSoup(html,'lxml')
 
-print('名为 title 的全部子节点：',soup.find_all('title'))
-print('title 子节点的文本内容：',soup.title.string)
-print('使用 get_text() 获取的文本内容：',soup.title.get_text())
+print('查找名为 title 的标签：',soup.select('title')) #标签名查找
+print('获取标签中的文字：',soup.select('title')[0].text) #text属性获取列表中标签元素的文字
+print('查找类名为 slogan 的标签：',soup.select('.slogan')) #类名查找
 
-target = soup.find_all('a',class_='next')
-print('CSS 类名匹配获取的节点：',target)
+# print('查找 id 名为 menu 的标签：',soup.select('#menu')) #id名查找
 
-target = soup.find_all(id='search')
-print('关键字 id 匹配获取的节点：',target)
+print('查找 <div>标签的类名为 introTxt 的标签：',soup.select('div.introTxt')) #组合查找
 
-target = soup.ul.find_all('a')
-print('第一个 ul 节点名称为 a 的节点：',target)
+print('查找父标签为 head 的 <title> 标签：',soup.select('head > title')) #子标签查找
 
-urls = []
-text = []
-for tag in target:
-    urls.append(tag.get('href'))
-    text.append(tag.get_text())
-print('a 节点的 href 属性值构成的列表：',urls)
-print('a 节点的文本内容构成的列表：',text)
+print('查找属性 class="logo" 的 <a> 标签：',soup.select('a[class="logo"]')) #使用属性查找
+
+print('获取标签中的属性值：',soup.select('a[class="logo"] > img')[0]['src']) #使用 [] 获取标签的属性值
+
 
