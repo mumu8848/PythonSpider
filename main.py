d174685675762a53506920f79abcdb7a9d796cf7
mumu8848 <mumu88008@163.com>
2024-11-13 12:25:15 +0800

    测试 re 库，python 中正则表达式的使用

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+import re
 import requests
 import chardet
 
@@ -10,22 +10,12 @@ rqg = requests.get(url,headers = ua)
 print('网站编码：',rqg.encoding)
 rqg.encoding = chardet.detect(rqg.content)['encoding']
 
+title_pattern = r'(?<=<title>).*?(?=</title>)'
+title_com = re.compile(title_pattern,re.M|re.S)
+title_search = re.search(title_com,rqg.text)
+title = title_search.group()
 
-html = rqg.text
-soup = BeautifulSoup(html,'lxml')
-
-print('查找名为 title 的标签：',soup.select('title')) #标签名查找
-print('获取标签中的文字：',soup.select('title')[0].text) #text属性获取列表中标签元素的文字
-print('查找类名为 slogan 的标签：',soup.select('.slogan')) #类名查找
-
-# print('查找 id 名为 menu 的标签：',soup.select('#menu')) #id名查找
-
-print('查找 <div>标签的类名为 introTxt 的标签：',soup.select('div.introTxt')) #组合查找
-
-print('查找父标签为 head 的 <title> 标签：',soup.select('head > title')) #子标签查找
-
-print('查找属性 class="logo" 的 <a> 标签：',soup.select('a[class="logo"]')) #使用属性查找
-
-print('获取标签中的属性值：',soup.select('a[class="logo"] > img')[0]['src']) #使用 [] 获取标签的属性值
+print('标题内容：',title)
+print('标题内容(findall()方法查找)：',re.findall(r'(<title>)(.*?)(</title>)',rqg.text))
 
 
