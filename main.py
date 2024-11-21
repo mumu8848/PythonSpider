11eca1a18f5c57c8b1adc44bf417dd9e8b5f36a0
mumu8848 <mumu88008@163.com>
2024-11-21 09:58:05 +0800

    selenium 使用 find_element 获取 52破解首页的第一个标题名称和属性，注意要学习 XPATH 的语法，平时可以使用浏览器的 f12 自己检索着练

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 option = webdriver.ChromeOptions()
 option.add_experimental_option('detach',True)
 browser = webdriver.Chrome(options=option)
-browser.get('https://www.baidu.com')
+
+browser.get('https://www.52pojie.cn/')
+elem = browser.find_element(By.XPATH,'//div[@id="nv"]/descendant::li/a[1]')
+print('定位到的元素返回值：',elem)
+print('元素的文本信息',elem.text)
+print('元素的属性值：',elem.get_attribute('href'))
