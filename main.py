cca2d439f5c0bbe840cd47bcf08c369d4294a30e
mumu8848 <mumu88008@163.com>
2024-11-22 14:39:04 +0800

    selenium 库的使用，豆瓣读书，提取多本与爬虫相关图书名称

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-import time
 
 option = webdriver.ChromeOptions()
 option.add_experimental_option('detach',True)
@@ -9,11 +10,18 @@ browser.get('https://book.douban.com/latest') # 中国出版集团
 
 browser.maximize_window()
 
-for i in range(5):
-    browser.execute_script('window.scrollBy(0,100)')
-    time.sleep(1)
+search_input = browser.find_element(By.XPATH,'//div[@class="nav-search"]//input')
+search_input.send_keys('爬虫')
+search_input.send_keys(Keys.ENTER)
 
-browser.save_screenshot('./tmp2/scrollBy.png')
+# 切换到最新的窗口
+handles = browser.window_handles
+browser.switch_to.window(handles[-1])
 
+# 定位最新窗口中的所有图书名所在的标签
+books = browser.find_elements(By.XPATH,'//div[@id="wrapper"]//div[@class="title"]//a')
+
+for book in books:
+    print(book.text)
 
 browser.quit()
