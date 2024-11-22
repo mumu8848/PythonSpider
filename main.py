06b603ca33cd2ae96daf2b9eaff0af6a95c11677
mumu8848 <mumu88008@163.com>
2024-11-22 11:50:51 +0800

    selenium 使用 request 获取原图 豆瓣新书图片 案例

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import requests
 
 option = webdriver.ChromeOptions()
 option.add_experimental_option('detach',True)
@@ -9,9 +10,14 @@ browser.get('https://book.douban.com/latest') # 中国出版集团
 pics = browser.find_elements(By.XPATH,'//div[@class="media__img"]//img')
 
 for index,pic in enumerate(pics):
-    pic_name = './tmp/'+str(index)+'.png'
+    pic_url = pic.get_attribute('src')
+    pic_name = './tmp1/'+ pic_url.split('/')[-1].split('/')[0] +'.png'
     print('正在存储图片：',pic_name)
+
+    # 使用 request 获取原图
+    resp = requests.get(pic_url)
+
     with open(pic_name,'wb') as f:
-        f.write(pic.screenshot_as_png)
+        f.write(resp.content)
 
 browser.quit()
