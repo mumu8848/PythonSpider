786ea52b4c01b25f1cf09889b660617e2a7f1d10
mumu8848 <mumu88008@163.com>
2024-11-22 11:03:26 +0800

    selenium 使用 find_elements 获取 豆瓣新书案例

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ option = webdriver.ChromeOptions()
 option.add_experimental_option('detach',True)
 browser = webdriver.Chrome(options=option)
 
-browser.get('http://cn.cnpubg.com/') # 中国出版集团
-elems = browser.find_elements(By.XPATH,'//div[@id="navCon"]/ul/descendant::a')
+browser.get('https://book.douban.com/latest') # 中国出版集团
+elems = browser.find_elements(By.XPATH,'//div[@id="content"]//li//a[@class="fleft"]')
 
 for elem in elems:
     print(elem.text)
