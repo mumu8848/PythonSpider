c8486fe6ce913b61f3f210e4d7c3d811f7f1b9a0
mumu8848 <mumu88008@163.com>
2024-11-22 14:08:30 +0800

    selenium 库的使用，元素交互--> 单击和输入

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import requests
+from selenium.webdriver.common.keys import Keys
 
 option = webdriver.ChromeOptions()
 option.add_experimental_option('detach',True)
 browser = webdriver.Chrome(options=option)
 
 browser.get('https://book.douban.com/latest') # 中国出版集团
-pics = browser.find_elements(By.XPATH,'//div[@class="media__img"]//img')
 
-for index,pic in enumerate(pics):
-    pic_url = pic.get_attribute('src')
-    pic_name = './tmp1/'+ pic_url.split('/')[-1].split('/')[0] +'.png'
-    print('正在存储图片：',pic_name)
+# 元素交互操作之--单击
+elem = browser.find_element(By.XPATH,'//div[@class="xbar"]//a')
+elem.click()
 
-    # 使用 request 获取原图
-    resp = requests.get(pic_url)
+# 元素交互操作之--输入文字
+search_input = browser.find_element(By.XPATH,'//div[@class="nav-search"]//input')
+search_input.send_keys('爬虫')
+search_input.send_keys(Keys.ENTER)
 
-    with open(pic_name,'wb') as f:
-        f.write(resp.content)
-
-browser.quit()
+# browser.quit()
