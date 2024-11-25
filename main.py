f2816d2185545b0c70d9a9ede1454f4a8d3c63bd
mumu8848 <mumu88008@163.com>
2024-11-25 09:34:23 +0800

    selenium 库的使用，微信读书，显式等待判断元素是否被定位到

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
 option = webdriver.ChromeOptions()
@@ -12,8 +15,14 @@ browser.get('https://weread.qq.com/web/category/newbook') # 中国出版集团
 
 # browser.maximize_window()
 
-elem = browser.find_element(By.XPATH,'//div[@class="ranking_page_content_list_container"]//a[1]')
-elem.click()
+elem = browser.find_element(By.XPATH,'//div[@class="ranking_page_content_list_container"]//a[@class="wr_bookList_item_link"][2]')
+
+try:
+    wait = WebDriverWait(browser,20,0.5)
+    wait.until(EC.element_to_be_clickable(elem))
+    elem.click()
+    time.sleep(5)
+finally:
+    browser.quit()
 
 
-# browser.quit()
