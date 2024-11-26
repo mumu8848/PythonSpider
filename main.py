dec08361fdcefe88ee450d15bde4954ad0c8263a
mumu8848 <mumu88008@163.com>
2024-11-26 17:07:15 +0800

    selenium 库的综合使用之获取百度标题栏文字和链接，并存入mongodb中

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pymongo
 
 option = webdriver.ChromeOptions()
 option.add_experimental_option('detach',True)
@@ -9,15 +12,22 @@ browser.implicitly_wait(10) # 隐式等待10s
 
 browser.get('https://www.baidu.com/') # 百度首页
 
-elems = browser.find_elements(By.XPATH,'//div[@id="s-top-left"]//a')
+# browser.maximize_window()
+
+elems = browser.find_elements(By.XPATH,'//div[@class="s-top-left-new s-isindex-wrap"]//a[@class="mnav c-font-normal c-color-t"]')
+
+# 建立本地链接，使用默认端口和用户名密码
+conn = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+mydb = conn['mydb'] # 创建数据库‘mydb’
+test = mydb['Baidu'] # 通过属性的方式创建集合
 
 for elem in elems:
-    if elem.text == '新闻':
-        elem.click()
+    link = elem.get_attribute('href')  # 获取链接
+    text = elem.text  # 获取文本内容
+    test.insert_one({'title':text, 'url': link})
 
-handle = browser.window_handles # 获取当前浏览器窗口句柄
-browser.switch_to.window(handle[-1]) # 将浏览器当前窗口切换成最新的窗口
 
-browser.save_screenshot('./tmp4/xinwen.png')
+for x in test.find():
+    print(x)
 
 browser.quit()
