2bfd8c80e70e8c2fa88a17965ae03d80d99f2a27
mumu8848 <mumu88008@163.com>
2024-11-27 15:53:21 +0800

    自动登录网站脚本之获取验证码，实现人工识别验证码，输入验证码之后登录成功。这里需要注意会话对象的保持。

--- GetKaptcha/get_kaptcha.py
+++ GetKaptcha/get_kaptcha.py
@@ -5,7 +5,9 @@ header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
                    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
 kaptcha_url = 'https://www.ptpress.com.cn/kaptcha.jpg' # 验证码图片地址
 
-r= requests.get(kaptcha_url,headers=header)
+session = requests.Session() # 会话对象能够跨请求保持某些参数，在这里是验证码
+
+r = session.get(kaptcha_url,headers=header)
 
 with open('./tmp/kaptcha.jpg','wb') as f:
     f.write(r.content)
@@ -17,3 +19,11 @@ im.show()
 kaptcha = input('请输入验证码：')
 
 print('获取的验证码为：',kaptcha)
+
+login_data = {'username':'18927565259','password':'@tipdm666','verifyCode':kaptcha}
+login_url = 'https://www.ptpress.com.cn/login'
+
+
+r = session.post(login_url,data=login_data)
+print('发送请求后返回的状态码为：',r.status_code)
+print('响应内容：',r.text)
