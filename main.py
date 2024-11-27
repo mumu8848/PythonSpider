0f574b7f5aadbd326eb087567f90412dd096d8a7
mumu8848 <mumu88008@163.com>
2024-11-27 14:01:39 +0800

    selenium 库的综合使用之泰迪内推岗位名称爬取

--- main.py
+++ main.py
@@ -10,21 +10,20 @@ browser = webdriver.Chrome(options=option)
 
 browser.implicitly_wait(10) # 隐式等待10s
 
-browser.get('https://www.baidu.com/') # 百度首页
+browser.get('https://www.5iai.com/#/index') # 百度首页
 
 # browser.maximize_window()
 
-elems = browser.find_elements(By.XPATH,'//div[@class="s-top-left-new s-isindex-wrap"]//a[@class="mnav c-font-normal c-color-t"]')
+elems = browser.find_elements(By.XPATH,'//div[@class="container"]//ul[@class="clearfix position"]//span[@class="left"]//span')
 
 # 建立本地链接，使用默认端口和用户名密码
 conn = pymongo.MongoClient('mongodb://127.0.0.1:27017')
 mydb = conn['mydb'] # 创建数据库‘mydb’
-test = mydb['Baidu'] # 通过属性的方式创建集合
+test = mydb['Jobs'] # 通过属性的方式创建集合
 
 for elem in elems:
-    link = elem.get_attribute('href')  # 获取链接
-    text = elem.text  # 获取文本内容
-    test.insert_one({'title':text, 'url': link})
+    Jobname = elem.text  # 获取文本内容
+    test.insert_one({'岗位名称': Jobname})
 
 
 for x in test.find():
