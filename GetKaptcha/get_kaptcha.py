30a152c7b92d0ed9a3b1c17cce56144be9a2f47e
mumu8848 <mumu88008@163.com>
2024-11-27 15:10:28 +0800

    自动登录网站脚本之获取验证码，使用 Image 库。直接使用pip install Image 下载即可

---
+++ GetKaptcha/get_kaptcha.py
@@ -0,0 +1,15 @@
+import requests
+from PIL import Image
+
+header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
+kaptcha_url = 'https://www.ptpress.com.cn/kaptcha.jpg' # 验证码图片地址
+
+r= requests.get(kaptcha_url,headers=header)
+
+with open('./tmp/kaptcha.jpg','wb') as f:
+    f.write(r.content)
+
+im = Image.open('./tmp/kaptcha.jpg')
+
+im.show()
